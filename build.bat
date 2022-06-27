abf7dc6b5286109a079fc56740a608d01e5c1cd7
tijani <tijani@3ec9de1e-4d70-418a-a75b-2ccc236fdfe6>
2022-06-27 02:31:09 +0000

    Set column limit in .clang-format to 120. resource.rc just contains keyboard shortcuts to close the window. On compilation, it shows a epilepsy inducing screen when the window is resized.

---
+++ build.bat
@@ -0,0 +1,39 @@
+@echo off
+
+set CC=cl
+set SHARED_FLAGS=/nologo /Feasake /std:c11 /W3 /FC
+
+if "%~1" == "release" (
+    set args=release
+) else (
+    set args=debug
+)
+
+if %args% == release (
+   echo "Release Build"
+   set BUILD_DIR=release
+   set BUILD_ARGS=/O2 /Os
+) else (
+   echo "Debug Build"
+   set BUILD_DIR=debug
+   set BUILD_ARGS=/Z7
+)
+
+RD /S /Q %BUILD_DIR%
+mkdir %BUILD_DIR%
+pushd %BUILD_DIR%
+
+REM Compile resources first
+echo ------------------------------
+echo Building Resources
+rc /nologo ..\src\resource.rc
+
+%CC% %SHARED_FLAGS% %BUILD_ARGS% ..\src\win32_asake.c kernel32.lib user32.lib gdi32.lib Shcore.lib /link ..\src\resource.res
+popd
+
+
+rem if %args% == release (
+
+rem ) else (
+
+rem )
---
+++ src/resource.h
@@ -0,0 +1,13 @@
+// NOTE: Fix ids to be closer to each other to prevent collisions with windows
+// defined IDS I don't think this matters but you never know with windows
+// ¯\_(ツ)_/¯
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#define IDC_STATIC -1
+#define IDI_ICON 1000
+#define ID_QUIT 1001
+
+
+#endif // RESOURCE_H
