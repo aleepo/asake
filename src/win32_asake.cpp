0750f238646535fb4da4c7370f8c5de30498ecd9
tijani <tijani@3ec9de1e-4d70-418a-a75b-2ccc236fdfe6>
2022-06-30 08:40:37 +0000

    cpp has some nice features, like not having to include a header file for bool.

--- src/win32_asake.cpp
+++ src/win32_asake.cpp
@@ -1,16 +1,19 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <shellscalingapi.h>
-#include <stdbool.h>
-#include <stdio.h>
+#include <stdint.h>
 #include <windows.h>
 
 #include "resource.h"
 
+static bool RUNNING;
 static BITMAPINFO BitmapInfo;
 static void *BitmapMemory;
-static HBITMAP BitmapHandle;
-static HDC BitmapDeviceContext;
+static int BitmapWidth;
+static int BitmapHeight;
+static int BytesPerPixels = 4;
+
+typedef uint8_t uint8;
 
 void ShowLastError(HWND Handle, LPCSTR ErrorMessage) {
   LPVOID lpMsgBuf;
@@ -21,36 +24,71 @@ void ShowLastError(HWND Handle, LPCSTR ErrorMessage) {
   }
 }
 
+static void RenderWeirdGradient(int XOffset, int YOffset) {
+  int Width = BitmapWidth;
+  int Height = BitmapHeight;
+
+  uint8_t *Row = (uint8_t *)BitmapMemory;
+  int Pitch = Width * BytesPerPixels;
+  for (int Y = 0; Y < BitmapHeight; ++Y) {
+    uint8_t *Pixel = (uint8_t *)Row;
+    for (int X = 0; X < BitmapWidth; ++X) {
+
+      // This is backwards because of little endian
+
+      // Blue
+      *Pixel = (uint8_t)(X + XOffset);
+      ++Pixel;
+
+      // Green
+      *Pixel = (uint8_t)(Y + YOffset);
+      ++Pixel;
+
+      // Red
+      *Pixel = 0;
+      ++Pixel;
+
+      // Padding
+      *Pixel = 0;
+      ++Pixel;
+    }
+    Row += Pitch;
+  }
+}
+
 // DIB stands for Device Independent Bitmap
 // Resize the DIB section or create one if it as never been made.
 static void Win32ResizeDIBSection(int Width, int Height) {
   // TODO(tijani): Bulletproof this.
   // Maybe don't free first, free after, then free first if that fails.
+  if (BitmapMemory) {
+    VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+  }
 
-  // TODO(tijani): Free the DIBSection
-  if (BitmapHandle) {
-    DeleteObject(BitmapHandle);
-  }
-  if (!BitmapDeviceContext) {
-    // TODO(tijani): Should this be recreated under certair circumstances like unpluging a monitor and pluging in a
-    // higher resolution one?
-    BitmapDeviceContext = CreateCompatibleDC(0);
-  }
+  BitmapWidth = Width;
+  BitmapHeight = Height;
 
   // BITMAPINFO BitmapInfo;
   BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-  BitmapInfo.bmiHeader.biWidth = Width;
-  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+  BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
   BitmapInfo.bmiHeader.biPlanes = 1;
-  BitmapInfo.bmiHeader.biBitCount = 32;
+  BitmapInfo.bmiHeader.biBitCount = 32; // using 32 because cpus are byte sized aligned.
   BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-  BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+  int BytesPerPixels = 4;
+  int BitmapMemorySize = (Width * Height) * BytesPerPixels;
+  BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+  RenderWeirdGradient(139, 0);
 }
 
-static void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height) {
-  StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS,
-                SRCCOPY);
+static void Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height) {
+  int WindowWidth = WindowRect->right - WindowRect->left;
+  int WindowHeight = WindowRect->bottom - WindowRect->top;
+
+  StretchDIBits(DeviceContext, 0, 0, BitmapWidth, BitmapHeight, X, Y, WindowWidth, WindowHeight, BitmapMemory,
+                &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT CALLBACK WndProc(HWND WindowHandle, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -88,12 +126,14 @@ LRESULT CALLBACK WndProc(HWND WindowHandle, UINT msg, WPARAM wParam, LPARAM lPar
   case WM_PAINT: {
     PAINTSTRUCT Paint;
     HDC DeviceContext = BeginPaint(WindowHandle, &Paint);
+    RECT ClientRect;
+    GetClientRect(WindowHandle, &ClientRect);
     int X = Paint.rcPaint.left;
     int Y = Paint.rcPaint.top;
     int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
     int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 
-    Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+    Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
     EndPaint(WindowHandle, &Paint);
   } break;
   default:
@@ -136,11 +176,22 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR Command, in
   // Load accelerator table
   haccel = LoadAccelerators(Instance, "IDR_MENU_ACCELERATOR");
 
-  MSG msg = {0};
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    if (!TranslateAccelerator(WindowHandle, haccel, &msg)) {
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
+  /* while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)){ */
+  /*    if (!TranslateAccelerator(WindowHandle, haccel, &msg)) { */
+  /*     TranslateMessage(&msg); */
+  /*     DispatchMessage(&msg); */
+  /*   } */
+  /* } */
+
+  RUNNING = true;
+  while (RUNNING) {
+    MSG msg = {0};
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+      if (!TranslateAccelerator(WindowHandle, haccel, &msg)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+      }
     }
   }
   return (int)(msg.wParam);
